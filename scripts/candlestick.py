a5d69a0881c3935dcf2e96e5c2628cb0c74d4c04
Jerry5704 <pawel.jozef.jurkiewicz@gmail.com>
2023-01-23 23:24:22 +0100

    stared candlestick classifier

--- scripts/candlestick.py
+++ scripts/candlestick.py
@@ -1,8 +1,15 @@
 class Candlestick():
+
     def __init__(self, date, open, high, low, close, volume):
         self.date = date
-        self.open = open
-        self.high = high
-        self.low = low
-        self.close = close
-        self.volume = volume
+        self.open = float(open.replace(',',''))
+        self.high = float(high.replace(',',''))
+        self.low = float(low.replace(',',''))
+        self.close = float(close.replace(',',''))
+        self.volume = float(volume.replace(',',''))
+
+        # Calculations
+        self.upper_shadow = self.high - self.close
+        self.body = abs(self.open - self.close)
+        self.type = ""
+        print(f"body lenght = {self.body}")
--- scripts/dataParser.py
+++ scripts/dataParser.py
@@ -33,11 +33,7 @@ class dataParser():
         elif month_name == "Dec":
             return 12
 
-    def __init__(self, raw_data, configParser):
-        self.raw_data = raw_data
-        self.configParser = configParser
-        self.frequency = self.get_frequency()
-        self.data_lines_list = [line for line in self.raw_data.text.split('\n')]
+    def get_candlesticks_list(self):
         list_of_candlesticks = []
         for line in self.data_lines_list[4:-1]:
             line_split = line.split(' ')
@@ -51,5 +47,39 @@ class dataParser():
             close = line_split[6]
             volume = line_split[8]
             list_of_candlesticks.append(Candlestick(day_date, open, high, low, close, volume))
-        for candlestick in list_of_candlesticks:
-            print(candlestick.date)
+        return list_of_candlesticks
+
+    # long body means that current candlestick body is at least twice as long as previous candlestick body
+    def get_is_long_body(self, candlestick, current_candlestick_index):
+        if self.candlesticks_list[current_candlestick_index].body > 2 * self.candlesticks_list[current_candlestick_index - 1].body:
+            return True
+        return False 
+
+    def classify_candlestick(self, candlestick, current_candlestick_index):
+        if current_candlestick_index == 0:
+            candlestick.type = "first"
+        else:
+            if self.get_is_long_body(candlestick, current_candlestick_index):
+                candlestick.type = "long"
+            else:
+                candlestick.type = "short"
+        current_candlestick_index += 1
+        return candlestick
+
+    def classify_candlesticks_list(self):
+        current_candlestick_index = 0
+        for candlestick in self.candlesticks_list:
+            self.classify_candlestick(candlestick, current_candlestick_index)
+            current_candlestick_index += 1
+    
+    def __init__(self, raw_data, configParser):
+        self.raw_data = raw_data
+        self.configParser = configParser
+        self.frequency = self.get_frequency()
+        self.data_lines_list = [line for line in self.raw_data.text.split('\n')]
+        self.candlesticks_list = self.get_candlesticks_list()
+        self.classify_candlesticks_list()
+        # print(self.candlesticks_list)
+        for candlestick in self.candlesticks_list:
+            print(candlestick.type)
+        
